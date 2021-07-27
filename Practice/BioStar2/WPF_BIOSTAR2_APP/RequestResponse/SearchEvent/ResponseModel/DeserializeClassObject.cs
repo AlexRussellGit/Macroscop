08cd9070eb0f286afb9311b3568e760dfddd2302
Alex <79187801+AlexRussellGit@users.noreply.github.com>
2021-07-27 16:37:10 +0500

    Add files via upload

---
+++ Practice/BioStar2/WPF_BIOSTAR2_APP/RequestResponse/SearchEvent/ResponseModel/DeserializeClassObject.cs
@@ -0,0 +1,76 @@
+﻿namespace WPF_BIOSTAR2_APPLICATION.RequestResponse.SearchEvent.ResponseModel
+{
+    using System;
+    public class DeserializeClassObject
+    {
+        public Eventcollection EventCollection { get; set; }
+        public Response Response { get; set; }
+    }
+
+    public class Eventcollection
+    {
+        public Row[] rows { get; set; }
+    }
+
+    public class Row
+    {
+        public string id { get; set; }
+        public DateTime server_datetime { get; set; }
+        public DateTime datetime { get; set; }
+        public string index { get; set; }
+        public string user_id_name { get; set; }
+        public User_Id user_id { get; set; }
+        public User_Group_Id user_group_id { get; set; }
+        public Device_Id device_id { get; set; }
+        public Event_Type_Id event_type_id { get; set; }
+        public string is_dst { get; set; }
+        public Timezone timezone { get; set; }
+        public string user_update_by_device { get; set; }
+        public string hint { get; set; }
+        public Door_Id[] door_id { get; set; }
+    }
+
+    public class User_Id
+    {
+        public string user_id { get; set; }
+        public string name { get; set; }
+        public string photo_exists { get; set; }
+    }
+
+    public class User_Group_Id
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Device_Id
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Event_Type_Id
+    {
+        public string code { get; set; }
+    }
+
+    public class Timezone
+    {
+        public string half { get; set; }
+        public string hour { get; set; }
+        public string negative { get; set; }
+    }
+
+    public class Door_Id
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Response
+    {
+        public string code { get; set; }
+        public string link { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ Practice/BioStar2/WPF_BIOSTAR2_APP/RequestResponse/SearchEvent/SearchEventHandler/Search.cs
@@ -0,0 +1,37 @@
+﻿namespace WPF_BIOSTAR2_APPLICATION.RequestResponse.SearchEvent
+{
+    using APIClassLibrary;
+    using WPF_BIOSTAR2_APPLICATION.Data.DataStructure.List;
+    using WPF_BIOSTAR2_APPLICATION.RequestResponse.SearchEvent.ResponseModel;
+    using Newtonsoft.Json;
+    using System;
+    using System.Windows;
+    public class Search
+    {
+        public static void SearchEventListResponse(Nullable<DateTime> firstdate, Nullable<DateTime> lastdate) // Получает Запрос в String и конвертирует в Obj (типа)
+        {
+            DeserializeClassObject MyObj = JsonConvert.DeserializeObject<DeserializeClassObject>(SystemInteraction.GetSearchEventResponseString(firstdate, lastdate));
+            SearchEventList.ClearInstance();
+            if (MyObj.EventCollection.rows == null)
+            {
+                MessageBox.Show("List is Empty!");
+            }
+            else
+            {
+                foreach (Row obj_row_field in MyObj.EventCollection.rows)
+                {
+                    SearchEventList.AddInstance(Convert.ToString(obj_row_field.datetime), DoorID_NameString(obj_row_field) , obj_row_field.device_id.id, obj_row_field.device_id.name, obj_row_field.user_group_id.name, obj_row_field.user_id.name, obj_row_field.event_type_id.code);
+                }
+            }
+        }
+
+        public static string DoorID_NameString(Row MyArr)
+        {
+            if (MyArr.door_id == null)
+            {
+                return null;
+            }
+            return MyArr.door_id[MyArr.door_id.Length-1].name;
+        }
+    }
+}
